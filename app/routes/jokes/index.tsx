ab6328ff9d5d3d0abaeeba6281f319e489eee0ae
Casey Kelly <casey@himaxwell.com>
2021-11-27 10:40:44 -0700

    add client and server validations to new joke form

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -5,19 +5,19 @@ import { db } from "~/utils/db.server"
 
 type LoaderData = { randomJoke: Joke }
 
-export let loader: LoaderFunction = async () => {
-  let count = await db.joke.count()
-  let randomRowNumber = Math.floor(Math.random() * count)
-  let [randomJoke] = await db.joke.findMany({
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count()
+  const randomRowNumber = Math.floor(Math.random() * count)
+  const [randomJoke] = await db.joke.findMany({
     take: 1,
     skip: randomRowNumber,
   })
-  let data: LoaderData = { randomJoke }
+  const data: LoaderData = { randomJoke }
   return data
 }
 
 export default function JokesIndexRoute() {
-  let data = useLoaderData<LoaderData>()
+  const data = useLoaderData<LoaderData>()
 
   return (
     <div>
